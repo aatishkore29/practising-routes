c31f781adeb62abf7a04957246a1e101a39c7219
Aatish Kore <aatishkore99@gmail.com>
2025-10-17 13:04:45 +0530

    Dynamic pages routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
+import Details from "./pages/app/details/Details";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Learn from "./pages/app/learn/Learn";
 
 function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      children: [
+        { index: true, element: <Hero /> },
+        {
+          path: "/courses",
+          children: [
+            { index: true, element: <Courses /> },
+            { path: ":courseId", element: <Details /> },
+          ],
+        },
+        { path: "/learn/:courseId", element: <Learn /> },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
+      <RouterProvider router={browserRouter} />
     </div>
   );
 }
--- src/pages/app/learn/Learn.jsx
+++ src/pages/app/learn/Learn.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import style from "./Learn.module.css";
 import coursesData from "../../../data/courses.json";
 // Task4: Import all the required elements from the react-router-dom
+import { useParams, Link } from "react-router-dom";
 
 function Learn() {
+  const { courseId } = useParams();
+  const course = coursesData.find((course) => course.id === courseId);
+  console.log(course);
+
   return (
     <div className={style.courses_container}>
       <div className={style.top_head}>
         {/* Task4: Create Link to go back to the Courses page */}
-        <h2 className={style.back}>{"<<"}</h2>
+        <Link to={`/courses/${course.id}`}>
+          <h2 className={style.back}>{"<<"}</h2>
+        </Link>
 
         {/* Task4: Title of the Course */}
-        <h1 className={style.heading}>**TITLE**</h1>
+        <h1 className={style.heading}>{course.title}</h1>
       </div>
       <div className={style.chapter_box}>
         <div className={style.chapters}>
@@ -19,12 +26,12 @@ function Learn() {
           <hr />
           <ul>{/*Task4: List of Chapters must be rendered here  */}</ul>
         </div>
-      
-      <div className={style.courses}>
-        {/**Task5:  Chapter Details Must be rendered here */}
+
+        <div className={style.courses}>
+          {/**Task5:  Chapter Details Must be rendered here */}
+        </div>
       </div>
     </div>
-</div>
   );
 }
 
